c81dd469ba56b53652c31b0e1c4c456b98ffb2f0
Sally Nader <sally.nader@vodafone.com>
2022-09-22 14:55:19 +0200

    solves missing var errors in bastion module

--- terraform/bastion/variables.tf
+++ terraform/bastion/variables.tf
@@ -1,9 +1,9 @@
 variable "key" {}
 variable "vpc_id" {}
-# variable "image_id" {}
+variable "image_id" {}
 variable "min_size" {}
 variable "max_size" {}
-# variable "template_name" {}
+variable "template_name" {}
 variable "instance_type" {}
 variable "desired_capacity" {}
 
