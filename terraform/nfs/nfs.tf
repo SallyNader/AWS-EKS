622dbf69a2b62721b3ff665dbe3ca728facad634
Sally Nader <sally.nader@vodafone.com>
2022-11-06 12:44:12 +0200

    adds depend on nfs

--- terraform/nfs/nfs.tf
+++ terraform/nfs/nfs.tf
@@ -17,6 +17,9 @@ resource "aws_efs_mount_target" "mount" {
   file_system_id  = aws_efs_file_system.efs.id
   subnet_id       = each.value
   security_groups = [aws_security_group.nfs.id]
+  depends_on = [
+    var.vpc_id
+  ]
 }
 
 resource "aws_security_group" "nfs" {
